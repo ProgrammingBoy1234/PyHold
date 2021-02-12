c9ff7003c442beb79572eb979c5dc96373748b1c
ProgrammingBoy1234 <78975217+ProgrammingBoy1234@users.noreply.github.com>
2021-02-12 23:08:38 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1'
+LONG_DESCRIPTION = 'Adds Ability Of Adding A Delay And A Sustain'
+
+# Setting up
+setup(
+    name="PyHold",
+    version=VERSION,
+    author="ProgrammingBoy1234",
+    author_email="<mail@neuralnine.com>",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
